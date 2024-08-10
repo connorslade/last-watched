921630924529cdec419851a7de9d3d5548022e29
Connor Slade <connor@connorcode.com>
2024-08-10 12:22:41 -0400

    More correct lua path joins

--- scripts/last-watched.lua
+++ scripts/last-watched.lua
@@ -12,11 +12,22 @@ function is_video_file(file)
     return false
 end
 
+function join_paths(path1, path2)
+    if path2:match("^[A-Za-z]:[/\\]") then
+        return path2
+    end
+
+    path1 = path1:gsub("[/\\]+$", "")
+    path2 = path2:gsub("^[/\\]+", "")
+    
+    return path1 .. "/" .. path2
+end
+
 function on_file_loaded(event)
     -- Get the extention and folder of the file
     local working_directory = mp.get_property("working-directory"):gsub("\\", "/")
     local file = mp.get_property("filename")
-    local path = working_directory .. "/" .. file
+    local path = join_paths(working_directory, mp.get_property("path"))
 
     local folder = path:match("^(.*[\\/])")
     local extention = file:match("^.+%.(.+)$")
@@ -28,7 +39,7 @@ function on_file_loaded(event)
     mp.osd_message("Marking as watched")
 
     -- Load the sidecar file
-    local sidecar_path = folder .. ".watched"
+    local sidecar_path = join_paths(folder, ".watched")
     local success, lines = pcall(io.lines, sidecar_path)
 
     -- Check for the current file in the sidecar file, returning if it is already there
